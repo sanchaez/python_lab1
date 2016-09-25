c6aada01468cece00b06cc4a3afd42862e280df5
Alexander Sh <sanchaez@hotmail.com>
2016-09-25 21:07:04 +0300

    Restructured & removed tty size feature

--- database.py
+++ database.py
@@ -1,54 +1,60 @@
 'Database module'
-from collections import Counter
+
 
 class TableDouble:
-    '''Authors table abstraction class. \ 
-Table is assured to contain no duplicates.'''
-    def __init__ (self, init_data=[], default_ratio=3):
-        'Init function'
-        #example data stored: {'Douglas Adams': 1, 'Stephen King': 2}
-        #table divisor (1/ratio). Used to print values.
+    """Authors table abstraction class.
+Table is assured to contain no duplicates."""
+
+    def __init__(self, init_data, default_ratio=10):
+        """Init function"""
+        from collections import Counter
+        # example data stored: {'Douglas Adams': 1, 'Stephen King': 2}
+        # table divisor (1/ratio). Used to print values.
         self.ratio = default_ratio
-        #Couter wrapper of dict used to autoincrement values
+        # Counter wrapper of dict used to autoincrement values
         self.data = Counter()
         for key in init_data:
             self.data[key] += 1
-            
+
     def add(self, key):
-        'Add value to table. Do nothing if it exists'
-        if name not in self.data:
+        """Add value to table. Do nothing if it exists"""
+        if key not in self.data:
             self.data[key] += 1
-        
+
     def find(self, search_key):
-        'Find value of a key in table.'
+        """Find value of a key in table."""
         return self.data[search_key]
-    
-    def findByID(self, search_value):
-        'Find a key by value (slow and linear).'
-        for key, value  in self.data.items():
+
+    def find_id(self, search_value):
+        """Find a key by value (slow and linear)."""
+        for key, value in self.data.items():
             if value == search_value:
-                return name
-    
+                return key
+    def filter(self):
+        """Add a filter & return TableDouble object"""
     def remove(self, key):
-        'Remove record by name'
+        """Remove record by name"""
         del self.data[key]
-        
-    def removeByID(self, value):
-        'Remove record by identifier'
-        del self.data[self.findByID[value]]
-        
+
+    def remove_id(self, value):
+        """Remove record by identifier"""
+        del self.data[self.find_id(value)]
+
     def __str__(self):
-        'String representation'        
-        #get terminal size for better output
-        rows, columns = subprocess.check_output(['stty', 'size']).split()
-        #divide into 2 columns
-        rows_1 = rows // ratio
+        """String representation"""
+        rows = 40
+        # following works in most linux systems:
+        # import subprocess
+        # rows = int(subprocess.check_output(['stty', 'size']).split()[0])
+        # divide into 2 columns
+        rows_1 = rows // self.ratio
         rows_2 = rows - rows_1
         result_string = rows * '*' + '\n'
         for name, identifier in self.data.items():
-          name_spaces = rows_2 - len(name)) // 2
-          id_spaces = (rows_1 - len(str(identifier))) // 2
-          result_string += '*' + id_spaces * ' ' + identifier + (id_spaces - 1) + '*'\
-            + (name_spaces - 1) + name + (name_spaces - 1) + '*\n' + rows * '*' + '\n'
+            name_spaces = (rows_2 - len(name)) // 2
+            id_spaces = (rows_1 - len(str(identifier))) // 2
+            # make a table string
+            result_string += ("*" + id_spaces * " " + "%d" + id_spaces * " " + "*"
+                             + (name_spaces - 1) * " " + name + name_spaces * " "
+                             + "*\n" + (rows * "*") + "\n") % identifier
         return result_string
-
