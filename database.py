9f7b746d4fdf9245088372a883322843035a78d6
Alexander Sh <sanchaez@hotmail.com>
2016-09-26 00:16:57 +0300

    Added 2nd dict for better value searching

--- database.py
+++ database.py
@@ -4,46 +4,66 @@
 class TableDouble:
     """Authors table abstraction class.
 Table is assured to contain no duplicates."""
-
-    def __init__(self, init_data, default_ratio=10):
+    def __init__(self, init_data):
         """Init function"""
         # example data stored: {'Douglas Adams': 1, 'Stephen King': 2}
-        # table divisor (1/ratio). Used to print values.
-        self.ratio = default_ratio
-        self.data = dict()
-        assert isinstance(init_data, object)
-        for key in init_data:
-            for i in range(1, len(init_data)):
-                self.data[key] = 1
+        # ratio is a table divisor (1/ratio). Used to print values.
+        self.ratio = 10
+        # rows is a number of rows used for printing values
+        # following works in most linux systems:
+        # import subprocess
+        # rows = int(subprocess.check_output(['stty', 'size']).split()[0])
+        self.rows = 40
+        self.data_name_id = dict()
+        self.data_id_name = dict()
+        self.add_list(init_data)
 
-    def add(self, key):
+    def add(self, identifier, name):
         """Add value to table. Do nothing if it exists"""
-        if key not in self.data:
-            self.data[key] += 1
-
-    def find(self, search_key):
-        """Find value of a key in table."""
-        return self.data[search_key]
-
-    def find_id(self, search_value):
-        """Find a key by value (slow and linear)."""
-        for key, value in self.data.items():
-            if value == search_value:
-                return key
+        assert type(identifier) is IntType, "id is not an integer: %r" % identifier
+        assert type(name) is StringType, "name is not a string: %r" % name
+        print name
+        print identifier
+        if name not in self.data_name_id:
+            self.data_name_id[name] = identifier
+            self.data_id_name[identifier] = name
 
     def add_list(self, name_list):
         # to perform all the nessesary restrictions
         for identifier, name in zip(range(len(name_list)), name_list):
             self.add(identifier, name)
+
+    def by_name(self, search_key):
+        """Find id of a key in table."""
+        assert type(search_key) is StringType, "name is not a string: %r" % search_key
+        identifier = self.data_name_id[search_key]
+        # to ensure consistence
+        assert self.data_id_name[identifier] == search_key
+        return identifier
+
+    def by_id(self, search_key):
+        """Find a key by value."""
+        assert type(search_key) is IntType, "id is not an integer: %r" % search_key
+        name = self.data_id_name[search_key]
+        # to ensure consistence
+        assert self.data_name_id[name] == search_key
+        return name
+
     def filter(self):
-        """Add a filter & return TableDouble object"""
+        """Return TableDouble object with applied filter"""
+        pass # not implemented
+
     def remove(self, key):
         """Remove record by name"""
-        del self.data[key]
+        val = self.data_name_id[key]
+        assert self.data_id_name[val] == key
+        del self.data_id_name[val], self.data_name_id[key]
 
-    def remove_id(self, value):
+    def remove_id(self, key):
         """Remove record by identifier"""
-        del self.data[self.find_id(value)]
+        val = self.data_id_name[key]
+        assert self.data_name_id[val] == key
+        del self.data_id_name[key], self.data_name_id[val]
 
     def __str__(self):
         """String representation"""
