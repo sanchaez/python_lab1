74335b6a84491b42dada6958e1a3a21b0845db79
Alexander Sh <sanchaez@hotmail.com>
2016-09-26 00:10:34 +0300

    Added big number test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from database import TableDouble
 def test_fn_1():
     """Docstring"""
     test_1 = TableDouble(['John doe', 'test1'])
+    test_1.add(1000000000000000000, "lol")
     print test_1
 
 test_fn_1()
