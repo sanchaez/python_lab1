ce195cd64efc0a37572531b4adb5ea45cfcbafbd
Alexander Sh <sanchaez@hotmail.com>
2016-10-04 02:22:04 +0300

    Reformated database.py

--- Lab1/database.py
+++ Lab1/database.py
@@ -1,102 +1,9 @@
+"""Database Module"""
+
 from types import *
 from tabulate import tabulate
-"""Database Module"""
 from collections import namedtuple
 
-
-# it is DEPRECATED and WILL BE REMOVED!
-# TODO: remove this class
-class TableDouble(object):
-    """Authors table abstraction class.
-Table is assured to contain no duplicates."""
-
-    def __init__(self, init_data):
-        """Init function
-            example data stored: {'Douglas Adams': 1, 'Stephen King': 2}"""
-        # ratio is a table divisor (1/ratio). Used to print values.
-        self.ratio = 10
-        # rows is a number of rows used for printing values
-        # following works in most linux systems:
-        # import subprocess
-        # rows = int(subprocess.check_output(['stty', 'size']).split()[0])
-        self.rows = 40
-        self.data_name_id = dict()
-        self.data_id_name = dict()
-        self.add_list(init_data)
-
-    def add(self, identifier, name):
-        """Add value to table. Do nothing if it exists"""
-        assert type(identifier) is IntType, "id is not an integer: %r" % identifier
-        assert type(name) is StringType, "name is not a string: %r" % name
-        if name not in self.data_name_id:
-            self.data_name_id[name] = identifier
-            self.data_id_name[identifier] = name
-
-    def add_list(self, name_list):
-        # to perform all the nessesary restrictions and prevent duplicates
-        for identifier, name in zip(range(len(name_list)), name_list):
-            self.add(identifier, name)
-
-    def by_name(self, search_key):
-        """Find id of a key in table."""
-        assert type(search_key) is StringType, "name is not a string: %r" % search_key
-        identifier = self.data_name_id[search_key]
-        # to ensure consistence
-        assert self.data_id_name[identifier] == search_key
-        return identifier
-
-    def by_id(self, search_key):
-        """Find a key by value."""
-        assert type(search_key) is IntType, "id is not an integer: %r" % search_key
-        name = self.data_id_name[search_key]
-        # to ensure consistence
-        assert self.data_name_id[name] == search_key
-        return name
-
-    def filter(self):
-        """Return TableDouble object with applied filter"""
-        pass  # not implemented
-
-    def remove(self, key):
-        """Remove record by name"""
-        val = self.data_name_id[key]
-        assert self.data_id_name[val] == key
-        del self.data_id_name[val], self.data_name_id[key]
-
-    def remove_id(self, key):
-        """Remove record by identifier"""
-        val = self.data_id_name[key]
-        assert self.data_name_id[val] == key
-        del self.data_id_name[key], self.data_name_id[val]
-
-    def print_this(self):
-        print self
-
-    def __str__(self):
-        """String representation"""
-        if self.data_id_name.items() == []:
-            return ""
-        else:
-            # divide into 2 columns
-            assert type(self.rows) is IntType, "rows is not an integer: %r" % self.rows
-            rows_1 = (self.rows // self.ratio)
-            rows_2 = self.rows - rows_1 - 3
-            result_string = self.rows * '*' + '\n'
-            for name, identifier in sorted(self.data_name_id.items()):
-                assert self.data_id_name[identifier] == name
-                id_len = len(str(identifier))
-                if id_len < rows_1:
-                    id_spaces = (rows_1 - id_len) // 2
-                    name_spaces = (rows_2 - len(name)) // 2 + 1
-                else:
-                    id_spaces = 0
-                    name_spaces = (self.rows - len(name) - (id_len) - 1) // 2
-
-                # make a string for each record
-                result_string += ("*" + id_spaces * " " + "%r" + id_spaces * " " + "*"
-                                  + name_spaces * " " + name + (name_spaces - int(0.5 + len(name) % 2)) * " "
-                                  + "*\n" + (self.rows * "*") + "\n") % identifier
-            return result_string
 
 class DatabaseTable(object):
     """A table container"""
@@ -107,6 +14,7 @@ class DatabaseTable(object):
     _default_output_type = 'grid'
     # counter
     _instance_count = 0
+
     def __init__(self, default_columns):
         """Init function"""
         self._instance_count += 1
@@ -125,7 +33,6 @@ class DatabaseTable(object):
         assert type(value) is ListType or TupleType
         self._indexed_table[key] = self._container_class._make(value)
 
-
     def __add__(self, other):
         if type(other) in (ListType, TupleType):
             self._indexed_table.append(self._container_class._make(other))
@@ -143,7 +50,7 @@ class DatabaseTable(object):
         return tabulate(self._indexed_table,
                         headers=self._container_class._fields,
                         tablefmt=self._default_output_type)
-    
+
     def subtable(self, **kwargs):
         """Creates a subtable object from this table.
         :kwargs:
@@ -200,7 +107,7 @@ class DatabaseTable(object):
             _type = kwargs['type']
         else:
             _type = self._default_join_type
-        #set
+        # set
         if _type == 'inner':
             _direction = 'full'
         elif _type != 'inner':
@@ -236,12 +143,12 @@ class DatabaseTable(object):
                             temporary_index_right_center[row_2].append(row_1)
                         else:
                             temporary_index_right_center[row_2] = [row_1]
-            #ANDing relations (intersection)
+            # ANDing relations (intersection)
             if temporary_index_copy:
-                temporary_index_left_center = {x:temporary_index_copy[x]
-                                                for x in temporary_index_left_center
-                                                if x in temporary_index_copy}
-            #copy
+                temporary_index_left_center = {x: temporary_index_copy[x]
+                                               for x in temporary_index_left_center
+                                               if x in temporary_index_copy}
+            # copy
             temporary_index_copy = temporary_index_left_center.copy()
 
         # outer join index
@@ -304,14 +211,15 @@ class DatabaseTable(object):
             gathered_table += row_1_data + row_2_data
         return gathered_table
 
+
 # container test
 if __name__ == '__main__':
     test_1 = DatabaseTable('a b')
     test_2 = DatabaseTable('c d')
-    for x, y in zip(range(1,5), range(4)):
+    for x, y in zip(range(1, 5), range(4)):
         test_1 += (x, y)
         test_2 += (y, x)
     print DatabaseTable._generic_join(test_1, test_2, ('a', 'c', lambda x, y: x == y))
     print DatabaseTable._generic_join(test_1, test_2, ('a', 'c', lambda x, y: x == y),
-                                      type='outer', direction='full', fields_1 = tuple('a'), fields_2 = tuple('c'))
+                                      type='outer', direction='full', fields_1=tuple('a'), fields_2=tuple('c'))
     print test_1.subtable(filter=lambda x: x > 2, filter_columns='a')
